a1d976f57802a9142d6524873dcb5acbb3428ee5
Quan Huynh <quanhuynh364@gmail.com>
2026-02-13 18:14:05 -0500

    settup mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-
+const mongoose = require('mongoose')
 const express = require('express')
 const workoutRoutes = require('./routes/workouts')
 
@@ -15,12 +15,22 @@ app.use((req,res,next)=>{
     next()
 })
 
+//routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for request 
-app.listen(process.env.PORT, ()=>{
-    console.log("Llistening on port!!!")
-    console.log("PORT is:", process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen for request 
+            app.listen(process.env.PORT, ()=>{
+                console.log("Listening on PORT!!!")
+                console.log("Port is: ",process.env.PORT)
+                console.log("Connect to DP")
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
 
 process.env
