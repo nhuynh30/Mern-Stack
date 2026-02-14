7afc9df2ed7b39bf8ac0adb5804c2ed8898ef60f
Quan Huynh <quanhuynh364@gmail.com>
2026-02-13 23:32:32 -0500

    added workoutController - organized

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 
+
 const workoutSchema = new Schema({
     title:{
         type: String,
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,21 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
 //Get all workouts
-router.get('/', (req, res)=>{
-    res.json({mssg: 'Get all workout'})
-})
+router.get('/', getWorkouts)
 
 //Get a single workout
-router.get('/:id', (req, res)=>{
-    res.json({mssg: 'Get a single workout'})
-})
+router.get('/:id', getWorkout)
 
 //Post a new workout
-router.post('/', async (req,res)=>{
-    const {title, load, reps} = req.body
-    
-    try{
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    }catch (error){
-        res.status(400).json({error: error.message})
-    }
-    
-})
+router.post('/', createWorkout)
 
 //delete a workout
 router.delete('/:id', (req, res)=>{
